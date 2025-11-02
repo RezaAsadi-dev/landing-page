848c4d93149970a635f5ada1fc971c923ecdfa71
Reza Asadi <88114772+RezaAsadi-dev@users.noreply.github.com>
2025-11-02 14:26:30 +0330

    Refactor HeroSpline component by removing Suspense for lazy loading and updating the scene path for improved performance and clarity.

--- src/components/sections/HeroSpline.tsx
+++ src/components/sections/HeroSpline.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense } from 'react';
+import { lazy } from 'react';
 
 const Spline = lazy(() => import('@splinetool/react-spline'));
 
@@ -9,18 +9,14 @@ type HeroSplineProps = {
 export default function HeroSpline({ onLoaded }: HeroSplineProps) {
   return (
     <div className="bg-white relative">
-      <Suspense fallback={
-        <div className="w-full  bg-gray-100 animate-pulse flex items-center justify-center">
-          <div className="text-gray-400 text-sm sm:text-base">در حال بارگذاری...</div>
-        </div>
-      }>
+    
         <div className="w-full  ">
           <Spline
-            scene="https://prod.spline.design/JW-BARweIA3SQb0p/scene.splinecode"
+            scene="/scene.splinecode"
             onLoad={onLoaded}
           />
         </div>
-      </Suspense>
+   
       <div className="bg-white absolute w-full h-[60px] sm:h-[50px] md:h-[60px] lg:h-[70px] bottom-[-5px] sm:bottom-[-20px] md:bottom-[-25px] shadow-[0_-20px_40px_30px_#fff] sm:shadow-[0_-30px_50px_40px_#fff] md:shadow-[0_-39px_66px_49px_#fff]"></div>
     </div>
   );
